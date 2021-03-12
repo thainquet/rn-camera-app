25ff62f298d45a77a78ac6b47cedffc21ee9e4df
Thai Nguyen <thainq00@gmail.com>
2021-03-12 11:11:20 +0700

    add firebase route

--- src/route.js
+++ src/route.js
@@ -3,6 +3,7 @@ import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreen from './screen/HomeScreen';
 import QRCode from './screen/QRCode';
 import Test from './screen/Test';
+import FirebaseData from './screen/FirebaseData';
 
 const Stack = createStackNavigator();
 
@@ -10,6 +11,7 @@ const Route = () => (
   <Stack.Navigator>
     <Stack.Screen name="Home" component={HomeScreen} />
     <Stack.Screen name="Scan" component={QRCode} />
+    <Stack.Screen name="FirebaseData" component={FirebaseData} />
     <Stack.Screen name="Test" component={Test} />
   </Stack.Navigator>
 );
